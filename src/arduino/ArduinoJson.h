9b086fe90c0d1605561b33e882e5d457a2ea4ff8
Zachary Westerman <westerman.zachary@gmail.com>
2026-06-17 21:10:47 -0500

    cleanup

--- src/arduino/ArduinoJson.h
+++ src/arduino/ArduinoJson.h
@@ -7,10 +7,6 @@
 typedef std::string JsonString;
 typedef std::map<std::string, nlohmann::json> JsonObject;
 
-// This is FUCKED tbh.
-#define as get
-#define isNull is_null
-
 class JsonDocument : public nlohmann::json {};
 
 class DeserializationError : public std::string {
