905600229ba2bef96bd82b57304763f406a80826
Zachary Westerman <westerman.zachary@gmail.com>
2026-06-20 18:29:35 -0500

    Add typedef for JsonArray

--- src/arduino/ArduinoJson.h
+++ src/arduino/ArduinoJson.h
@@ -3,10 +3,12 @@
 #include "../lib/json.hpp"
 #include <string>
 #include <map>
+#include <vector>
 
 typedef std::string JsonString;
 typedef std::map<std::string, nlohmann::json> JsonObject;
 typedef nlohmann::json JsonDocument;
+typedef std::vector<nlohmann::json> JsonArray;
 
 class DeserializationError : public std::string {
     public:
