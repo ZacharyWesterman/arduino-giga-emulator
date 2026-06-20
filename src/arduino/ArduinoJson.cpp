09b0cd46ae280fbd8d838213fa2f5e60772c8a8e
Zachary Westerman <westerman.zachary@gmail.com>
2026-06-20 17:19:18 -0500

    handle json deserialization errors correctly

--- src/arduino/ArduinoJson.cpp
+++ src/arduino/ArduinoJson.cpp
@@ -1,7 +1,11 @@
 #include "ArduinoJson.h"
 
 DeserializationError deserializeJson(JsonDocument& document, const char* text) {
-    document = nlohmann::json::parse(text);
+    try {
+        document = nlohmann::json::parse(text);
+    } catch(std::exception e) {
+        return DeserializationError{e.what()};
+    }
 
     return DeserializationError{};
 }
