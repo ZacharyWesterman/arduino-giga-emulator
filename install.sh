6ea8898a3cbda564f58da444b6d2fde802a4cb50
Zachary Westerman <westerman.zachary@gmail.com>
2026-06-26 20:16:01 -0500

    make install script a little more robust

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+if [ ! -d ~/.local ]; then
+	echo >&2 "ERROR: Unable to create symlink in" ~/.local/bin
+	exit 1
+fi
+
+[ ! -d ~/.local/bin ] && mkdir ~/.local/bin
+
 [ -L ~/.local/bin/"$1" ] && rm -f ~/.local/bin/"$1"
 
 ln -s "$(pwd)/emulate" ~/.local/bin/"$1"
